81815844843a9b7368fbc21bf484d2a80436465d
Chien <chien@gmail.com>
2017-07-02 09:46:34 +0800

    2017070201

---
+++ brad2101/src/tw/brad/j2ee/Brad13.java
@@ -0,0 +1,39 @@
+package tw.brad.j2ee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet("/Brad13")
+@MultipartConfig
+public class Brad13 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		request.setCharacterEncoding("UTF-8");
+		
+		String uploadPath = getServletContext().getInitParameter("upload-path");
+//		Part part = request.getPart("upload");
+//		String filename = part.getSubmittedFileName();
+//		part.write(uploadPath + "/" + filename);
+		
+		out.print(uploadPath + "<br>");
+//		out.print(filename + "<br>");
+//		out.print("Upload Success");
+	}
+
+}
